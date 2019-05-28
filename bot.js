1a50072f7cf9e88ecc98e7f452747396b50ee1f4
96Decibel <49520814+96Decibel@users.noreply.github.com>
2019-05-28 22:57:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,136 +1,80 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs"); 
-var prefix = "$";
 
 
-
-
-client.on('ready', function(){
-    var ms = 60000 ;
-    var setGame = ['$help | by RateED','Ramadan Kareem ??'];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/barontube`);
-    }, ms);
- console.log(`${client.user.tag} Ready !`);
-});
-
-
-
-const credits = JSON.parse(fs.readFileSync("./credits.json", "utf8"));
-const coolDown = new Set();
-
-client.on('message',async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === 'dm') return;
- 
-  const args = message.content.split(' ');
-  const credits = require('./credits.json');
-  const path = './credits.json';
-  const mention = message.mentions.users.first() || client.users.get(args[1]) || message.author;
-  const mentionn = message.mentions.users.first() || client.users.get(args[1]);
-  const author = message.author.id;
-  const balance = args[2];
-  const daily = Math.floor(Math.random() * 350) + 10;
- 
-  if(!credits[author]) credits[author] = {credits: 50};
-  if(!credits[mention.id]) credits[mention.id] = {credits: 50};
-  fs.writeFile(path, JSON.stringify(credits, null, 5), function(err) {if(err) console.log(err)});
+ const devs1 = ["552477722962558976"]
+client.on("message",async message => {
+  if(message.content === '#pl'){//البلاينق
+      if (!devs1.includes(message.author.id)) return message.reply('**أنت لا تمتلك صلاحيات في البوت للأسف**')
  
  
-  if(message.content.startsWith(prefix + "credit" || prefix + "credits")) {
-  if(args[0] !== `${prefix}credit` && args[0] !== `${prefix}credits`) return;
  
-  if(args[2]) {
-    if(isNaN(args[2]) || args[2] < 0) return message.channel.send(`:interrobang: **| ${message.author.username}, type the credit you need to transfer! **`);
-    if(mention.bot) return message.channel.send(`**:heavy_multiplication_x:| ${message.content.split(' ')[1]} لم يتم العثور على**`);
-    if(mention.id === message.author.id) return message.channel.send('**:heavy_multiplication_x:| لا يمكنك تحويل كردت لنفسك**');
-    if(credits[author].credits < balance) return message.channel.send(`** :thinking: | ${message.author.username}, Your balance is not enough for that!**`);
-    var one = Math.floor(Math.random() * 9) + 1;
-    var two = Math.floor(Math.random() * 9) + 1;
-    var three = Math.floor(Math.random() * 9) + 1;
-    var four = Math.floor(Math.random() * 9) + 1;
+      let shop = '';
+        let fillter = m => m.author.id === message.author.id
  
-    var number = `${one}${two}${three}${four}`;
-   
-    message.channel.send(`**:heavy_dollar_sign:| \`${number}\`, أكتب الرقم للأستمرار**`).then(m => {
-      message.channel.awaitMessages(m => m.author.id === message.author.id, {max: 1, time: 10000}).then(c => {
-        if(c.first().content === number) {
-          m.delete();
-          message.channel.send(`**:moneybag: | ${message.author.username}, has transferred \`${balance}\` to ${mention}**`);
-          credits[author].credits += (-balance);
-          credits[mention.id].credits += (+balance);
-          fs.writeFile(path, JSON.stringify(credits, null, 5), function(err) {if(err) console.log(err)});
-        } else if(c.first().content !== number) {
-          m.delete();
-          message.channel.send(`** :money_with_wings: | تم الغاء الإرسال**`);
-        }
-      });
-    });
-  }
-  if(!args[2]) {
-    if(mention.bot) return message.channel.send(`:interrobang:**| ${message.author.username}, I cant find** ${message.content.split(' ')[1]}**!**`);
-   message.channel.send(`**${mention.username}, your :credit_card: balance is** \`$${credits[mention.id].credits}\`**.** `);
- }
- 
- }
-       if(args[0].toLowerCase() === `$daily`) {  
-     
-if(credits[message.author.id].daily != moment().format('L')) {
- 
-      credits[message.author.id].daily = moment().format('L');
-         
- 
-         let ammount = (300, 500, 100, 200, 120, 150, 350, 320,220,250);
-         credits[author].credits += ammount;
-     
+             message.channel.send("**قم بكتابة البلاينق**").then(e => {
+             message.channel.awaitMessages(fillter, { time: 60000, max: 1                                    
+             })
+       .then(co => {
+         shop = co.first().content;
+          co.first().delete();
        
-         message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: ${ammount} daily credits!**`);
-       fs.writeFile("./credits.json", JSON.stringify(credits), function(e) {
-           if (e) throw e;
+       client.user.setGame(shop);
+ 
+   
        })
+             })
+  }
+  });
  
-     }else{
-     message.channel.send(`:stopwatch: : **Please cool down  ${moment().endOf('day').fromNow()}**`);
  
-     }
+client.on("message",async message => {
+  if(message.content === '#wt'){//واتشنق
+  if (!devs1.includes(message.author.id)) return message.reply('**أنت لا تمتلك صلاحيات في البوت للأسف**')
  
-       }
-});
+ 
+      let shwop = '';
+        let fillter = m => m.author.id === message.author.id
+ 
+             message.channel.send("**قم بكتابة الواتشينق**").then(e => {
+             message.channel.awaitMessages(fillter, { time: 60000, max: 1                                    
+             })
+       .then(co => {
+         shop = co.first().content;
+          co.first().delete();
+       // 90 + 40 + 20 + 20 + 10 + 20 = 200
+         client.user.setActivity(shwop, {type:'WATCHING'});
+       
+ 
+   
+       })
+             })
+  }
+  });
+ 
+client.on("message",async message => {
+  if(message.content === '#st'){//ستريمنق
+  if (!devs1.includes(message.author.id)) return message.reply('**أنت لا تمتلك صلاحيات في البوت للأسف**')
+ 
+ 
+      let shqop = '';
+        let fillter = m => m.author.id === message.author.id
+ 
+             message.channel.send("**قم بكتابة الستريمنق**").then(e => {
+             message.channel.awaitMessages(fillter, { time: 60000, max: 1                                    
+             })
+       .then(co => {
+         shop = co.first().content;
+          co.first().delete();
+       // 90 + 40 + 20 + 20 + 10 + 20 = 200
+         client.user.setActivity(shqop,"https://www.twitch.tv/RATEED"});
+       
+ 
+   
+       })
+             })
+  }
+  });
 
-client.on('message', async message => {
-    let amount = 200;
-    if(message.content.startsWith(prefix + "daily")) {
-    if(message.author.bot) return;
-    if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
-    
-    let userData = credits[message.author.id];
-    let m = userData.credits + amount;
-    credits[message.author.id] = {
-    credits: m
-    };
-
-    fs.writeFile("./credits.json", JSON.stringify(userData.credits + amount), (err) => {
-    if (err) console.error(err);
-    });
-    
-    message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: ${amount} credits!**`).then(() => {
-        coolDown.add(message.author.id);
-    });
-    
-    setTimeout(() => {
-       coolDown.remove(message.author.id);
-    },86400000);
-    }
-});
 
 client.login(process.env.BOT_TOKEN);
